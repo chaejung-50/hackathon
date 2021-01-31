d470eb0cbfa1cad3837454f5d2c50085999a77b0
Benjamin Huynh <ben.huynh408@gmail.com>
2021-01-30 19:53:42 -0800

    Added weather checker - Derek Nguyen

--- receive_sms.py
+++ receive_sms.py
@@ -1,21 +1,31 @@
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+import getWeather
 
 app = Flask(__name__)
-
+weatherflag = 0
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
+    global weatherflag
 	# Get the message the user sent to Twilio number
-  body = request.values.get('Body', None)
+    body = request.values.get('Body', None)
 
-  resp = MessagingResponse()
+    resp = MessagingResponse()
 
-  if body == 'hi I need attention':
-  	resp.message("I'm giving you attention... what do you want")
-  else if body == "hehe I'm kidding xD":
-  	resp.message("you needy person smh")
+    if weatherflag == 1:
+        weather = getWeather.api_response(body)
+        resp.message(weather)
+        weatherflag = 0
+
+    elif 'weather' in body.lower():
+        resp.message("Where are you From?")
+        weatherflag=1
+
+    elif body == 'bye':
+        resp.message("Goodbye")
+
   
-  return str(resp)
+    return str(resp)
  
 if __name__ == "__main__":
   app.run(debug=True)
