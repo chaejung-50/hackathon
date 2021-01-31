4fb10eee67b56e0c2f2eddb597c84d049f15b594
angelguajardo <41927488+angelguajardo@users.noreply.github.com>
2021-01-30 19:28:09 -0600

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,14 +1,24 @@
 from twilio.rest import Client
 import credentials
+import random 
+import schedule
 
+Morning_Stuff = ["i love you biiiiitch, i ain't never gonna stop loving you biiiiiitch", 
+                 "Morning beautiful",
+                "Wake up bihhh"]
 account_sid = credentials.account_sid
 auth_token = credentials.auth_token
 
-client = Client(account_sid, auth_token)
+def send_mess(quote):
+    client = Client(account_sid, auth_token)
+    client.messages.create(
+        to = credentials.my_cell,
+        from_= credentials.my_twilio,
+        body=quote)
 
-message = client.messages.create(
-    to = credentials.my_cell,
-    from_= credentials.my_twilio,
-    body="i love you biiiiitch, i ain't never gonna stop loving you biiiiiitch")
+quote = Morning_Stuff[random.randint(0, len(Morning_Stuff))]
+schedule.every().day.at("8:00").do(send_mess, Morning_Stuff[0])
 
-print(message.sid)
+while True:
+    schedule.run_pending()
+    time.sleep(2)
