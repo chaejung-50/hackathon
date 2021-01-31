9522cda37d527e16da8f2659aeb2c9f5df8a6047
Benjamin Huynh <ben.huynh408@gmail.com>
2021-01-30 19:33:17 -0800

    Made the input dynamic

--- receive_sms.py
+++ receive_sms.py
@@ -4,9 +4,16 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
+def incoming_sms():
+	# Get the message the user sent to Twilio number
+  body = request.values.get('Body', None)
+
   resp = MessagingResponse()
-  resp.message("I do not love you")
+
+  if body == 'hi I need attention':
+  	resp.message("I'm giving you attention... what do you want")
+  else if body == "hehe I'm kidding xD":
+  	resp.message("you needy person smh")
   
   return str(resp)
  
